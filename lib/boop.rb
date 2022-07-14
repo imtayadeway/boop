889652ffbd2fba8b9b8605d1e5b50af364b867ea
Tim Wade <hello@timjwade.com>
2022-07-14 10:53:08 -0700

    Add a cache store

---
+++ lib/boop.rb
@@ -0,0 +1,40 @@
+module Boop
+  class CacheStore
+    CACHE_TTL = 125.seconds # slightly more than 2 minutes
+    RACE_CONDITION_TTL = 5.seconds
+
+    ConnectionError = Class.new(StandardError)
+
+    @@cache = ActiveSupport::Cache::RedisCacheStore.new(
+      namespace: "boop",
+      expires_in: CACHE_TTL,
+      race_condition_ttl: RACE_CONDITION_TTL,
+      reconnect_attempts: 1,
+      url: "redis://boop:1234",
+      error_handler: -> (method:, returning:, exception:) {
+        if Redis::BaseConnectionError === exception
+          raise ConnectionError
+        end
+      }
+    )
+
+    attr_reader :nonce
+
+    def self.store(thing)
+      @@cache.write(thing, "")
+    rescue ConnectionError
+      nil
+    end
+
+    def self.consume(thing)
+      if @@cache.exist?(thing)
+        @@cache.delete(thing)
+        true
+      else
+        false
+      end
+    rescue ConnectionError
+      true # fail closed if Redis becomes unavailable
+    end
+  end
+end
